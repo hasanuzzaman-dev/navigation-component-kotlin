42b70bebe03ea95464673bec8bfe2048526182b6
hasanuzzman-dev <hasanuzzaman.dev@gmail.com>
2021-04-07 00:05:21 +0600

    fragment add

---
+++ app/src/main/java/com/hasan/navigation/fragments/ChooseReceiverFragment.kt
@@ -0,0 +1,7 @@
+package com.hasan.navigation.fragments
+
+import androidx.fragment.app.Fragment
+import com.hasan.navigation.R
+
+
+class ChooseReceiverFragment : Fragment(R.layout.fragment_choose_receiver)
---
+++ app/src/main/java/com/hasan/navigation/fragments/HomeFragment.kt
@@ -0,0 +1,10 @@
+package com.hasan.navigation.fragments
+
+import androidx.fragment.app.Fragment
+import com.hasan.navigation.R
+
+
+class HomeFragment : Fragment(R.layout.fragment_home)
+
+
+

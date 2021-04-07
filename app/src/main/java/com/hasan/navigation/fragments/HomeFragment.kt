20dba7645e212499f679073d4a1c8d96c2a72692
hasanuzzman-dev <hasanuzzaman.dev@gmail.com>
2021-04-07 23:24:36 +0600

    add anim using navOption

--- app/src/main/java/com/hasan/navigation/fragments/HomeFragment.kt
+++ app/src/main/java/com/hasan/navigation/fragments/HomeFragment.kt
@@ -3,6 +3,7 @@ package com.hasan.navigation.fragments
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
 import com.hasan.navigation.R
 import kotlinx.android.synthetic.main.fragment_home.*
@@ -12,7 +13,6 @@ class HomeFragment : Fragment(R.layout.fragment_home){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         val navController = findNavController()
 
         btn_view_balance.setOnClickListener{
@@ -34,7 +34,7 @@ class HomeFragment : Fragment(R.layout.fragment_home){
             navController.navigate(action)
 
             // Navigate using direct action id
-            //navController.navigate(R.id.action_homeFragment_to_viewTransactionFragment)
+            /* navController.navigate(R.id.action_homeFragment_to_viewTransactionFragment)*/
         }
 
         btn_send_money.setOnClickListener{
@@ -43,7 +43,23 @@ class HomeFragment : Fragment(R.layout.fragment_home){
             navController.navigate(action)
 
             // Navigate using direct action id
+
             //navController.navigate(R.id.action_homeFragment_to_chooseReceiverFragment)
+
+            // Navigate using destination Id (1)
+            // If you using destination id then its nav graph animation did not work
+            // you need to create animation here
+
+            /*val navOption = NavOptions.Builder()
+                .setEnterAnim(R.anim.slide_in_right)
+                .setExitAnim(R.anim.slide_out_right)
+                .setPopEnterAnim(R.anim.slide_in_left)
+                .setPopExitAnim(R.anim.slide_out_right)
+                .build()
+
+            navController.navigate(R.id.chooseReceiverFragment, null, navOption)*/
+
+            // navController.navigate(R.id.chooseReceiverFragment)
         }
     }
 }
